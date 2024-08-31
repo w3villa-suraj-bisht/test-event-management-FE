04642fe4ccb4d5da214ef1435935fa7bd94c56d5
w3villa-Aakansha-Sharma <aakansha.sharma@w3villa.com>
2024-08-31 12:15:35 +0530

    signin

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,26 +1,48 @@
 "use client";
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-// api service
-
-
-
 const SignIn = () => {
-  const router = useRouter();
+  
 
 
-
-  useEffect(() => {
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
   return (
-    <div className="bg-[#f1f1f1]">
-      <div className="container mx-auto p-4 flex justify-center items-center min-h-screen ">
-        <div className="w-full max-w-md" >
-          hello world
-        </div>
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+        <form autoComplete="off">
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
+              Email:
+            </label>
+            <input
+              type="email"
+             
+              id="email"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
+              Password:
+            </label>
+            <input
+              type="password"
+             
+              id="password"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300 ease-in-out"
+          >
+            Login
+          </button>
+          <p className="text-center mt-2">Don't have an account <a href="/signup" className="text-blue-500">Signup</a></p>
+        </form>
       </div>
     </div>
   );
